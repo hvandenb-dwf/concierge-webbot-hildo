94125ecde7beb10ff90ea5cddc1abf18036dafc4
Hildo van den Berg <hvandenb@gmail.com>
2025-05-15 23:04:12 +0200

    Minimal working version with FastAPI root and static page

--- app/main.py
+++ app/main.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from app.bot_logic import get_bot_reply
-from app.tts import text_to_speech
-from app.cloudinary_util import upload_audio
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def serve_frontend():
+def root():
     return FileResponse("static/index.html")
-
-@app.post("/ask")
-async def ask(request: Request):
-    data = await request.json()
-    user_input = data.get("text")
-    bot_reply = get_bot_reply(user_input)
-    audio_path = text_to_speech(bot_reply)
-    audio_url = upload_audio(audio_path)
-    return {"reply": bot_reply, "audio_url": audio_url}
